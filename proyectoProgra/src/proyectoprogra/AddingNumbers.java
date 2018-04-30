9142a86515b2deaa9e22886013563db9e5d2865a
yeine <yeine@LAPTOP-LH79V30I>
2018-04-29 21:04:06 -0500

    Comentarios y declaracion de la variable de total de sumas

--- proyectoProgra/src/proyectoprogra/AddingNumbers.java
+++ proyectoProgra/src/proyectoprogra/AddingNumbers.java
@@ -4,13 +4,14 @@ package proyectoprogra;
  */
 public class AddingNumbers
 {
-    private int correctSums;
-    private boolean controlSums;
-    private int failedsums;
+    private int correctSums;//Este contador contiene la cantidad de sumas corretcas
+    private boolean controlSums;//Este boolean controla que se cumplan las 15 sumas para que se termine el juego
+    private int totalSums;//Total de sumas realizadas
+    private int failedSums;//Este contador lleva la cantidad de sumas incorrectas
     
     public int getRandomNumber ()
     {
-        return (int) (Math.random()*8) + 1;
+        return (int) (Math.random()*8) + 1;//Determina numeros al azar
     }
     
     public int addingTheFirstNumber ()
