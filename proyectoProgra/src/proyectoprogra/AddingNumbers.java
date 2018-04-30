15695eac1798b4576499d435b1d4c3abb79de403
Faigda <jasson.rojasmadriz99@hotmail.es>
2018-04-29 20:01:56 -0600

    Nuevos metodos para que el usuario ingrese la suma del primer o segundo numero

--- proyectoProgra/src/proyectoprogra/AddingNumbers.java
+++ proyectoProgra/src/proyectoprogra/AddingNumbers.java
@@ -13,4 +13,19 @@ public class AddingNumbers
         return (int) (Math.random()*8) + 1;
     }
     
+    public int addingTheFirstNumber ()
+    {  
+        int number1=0, number2=0, sum=0;
+        
+        return sum;
+    }
+    
+    public int addingTheSecondNumber()
+    {    
+        int number1=0, number2=0, sum=0;
+        
+        
+        return sum;
+    }
+    
 }
