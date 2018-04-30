c506ad753508cb50d30d318e042146c6b676cd5a
ZeroKoriel <36462249+ZeroKoriel@users.noreply.github.com>
2018-04-29 21:20:59 -0600

    Método de cálculo de suma terminado. Y comentarios añadidos. Borrado de parámetros inecesarios del método calculateSum.

--- proyectoProgra/src/proyectoprogra/AddingNumbers.java
+++ proyectoProgra/src/proyectoprogra/AddingNumbers.java
@@ -1,58 +1,63 @@
 package proyectoprogra;
 
 import javax.swing.JOptionPane;
-
 /*
  * @authors Yeiner, Jasson y Edwin.
  */
-public class AddingNumbers {
-
+public class AddingNumbers
+{
     private int correctSums;//Este contador contiene la cantidad de sumas corretcas.
     private boolean controlSums = true;//Este boolean controla que se cumplan las 15 sumas para que se termine el juego.
     private int totalSums;//Total de sumas realizadas.
     private int failedSums;//Este contador lleva la cantidad de sumas incorrectas.
 
-    public int getRandomNumber() {
+    //Creación del objeto control, para llamar a los metodos adding.
+    AddingNumbers control = new AddingNumbers();
+
+    public int getRandomNumber()
+    {
         return (int) (Math.random() * 8) + 1;//Determina numeros al azar.
     }
 
     //Se obtiene el primer número.
-    public int addingTheFirstNumber() {
-        int number1 = getRandomNumber();
-
-        return number1;
+    public int addingTheFirstNumber()
+    {
+        return getRandomNumber();
     }
 
     //Se obtiene el segundo número.
-    public int addingTheSecondNumber() {
-        int number2 = getRandomNumber();
-
-        return number2;
+    public int addingTheSecondNumber() 
+    {
+        return getRandomNumber();
     }
 
     //Método de la suma de los números aleatorios.
-    public void calculateSum(boolean controlSums, int addingTheFirstNumber, int addingTheSecondNumber) {
-        
-        //Creación del objeto control, para llamar a los metodos adding.
-        AddingNumbers control = new AddingNumbers();
+    public void calculateSum()
+    {
+        //Almacenamiento de los números aleatorios en variables con llamadas a los métodos adding
         int firstRandom = control.addingTheFirstNumber();
         int secondRandom = control.addingTheSecondNumber();
-        int outputSum = firstRandom+secondRandom;
-        int number1;
-        int ingresedNumber1;
-
+        
+        //Suma de de los números aleatorios.
+        int outputSum = firstRandom + secondRandom;
+        
+        //Ciclo que calcula la cantidad de sumas acertadas y falladas.
         while (controlSums) 
         {
-            ingresedNumber1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero"));
-            if (ingresedNumber1+secondRandom==outputSum) 
+            int ingresedNumber1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero"));
+            if (ingresedNumber1 + secondRandom == outputSum) 
             {
                 correctSums++;
-                
-                
-            }else
+                //Modelo para la interfaz
+                JOptionPane.showMessageDialog( null, "Tiene "+correctSums+" sumas acertardas");
+                if (correctSums == 15)
+                {
+                    controlSums = false;
+                }
+            } else
             {
                 failedSums++;
-            
+                JOptionPane.showMessageDialog( null, "Tiene "+failedSums+" sumas fallidas");
             }
         }
     }
