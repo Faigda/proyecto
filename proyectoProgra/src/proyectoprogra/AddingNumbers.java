1f3de78074474f92b59042dc79800297a40066dc
yeine <yeine@LAPTOP-LH79V30I>
2018-04-29 20:46:40 -0500

    Declaracion variables de adding numbers

--- proyectoProgra/src/proyectoprogra/AddingNumbers.java
+++ proyectoProgra/src/proyectoprogra/AddingNumbers.java
@@ -6,9 +6,11 @@ public class AddingNumbers
 {
     private int correctSums;
     private boolean controlSums;
+    private int failedsums;
     
     public int getRandomNumber ()
     {
         return (int) (Math.random()*8) + 1;
     }
+    
 }
