a3fca360732f6e776a32ddac08aa1aea8a484d8a
yeine <yeine@LAPTOP-LH79V30I>
2018-05-20 19:37:40 -0500

    Se agegaron las instrucciones en ingles del juego sumando numeros a la interfaz

--- proyectoProgra/src/GameInterfaces/AddingNumbersInterface.java
+++ proyectoProgra/src/GameInterfaces/AddingNumbersInterface.java
@@ -6,6 +6,7 @@
 package GameInterfaces;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import proyectoprogra.AddingNumbers;
 
 /**
@@ -97,6 +98,11 @@ public class AddingNumbersInterface extends javax.swing.JFrame {
         helpButton2.setBackground(new java.awt.Color(0, 153, 153));
         helpButton2.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
         helpButton2.setText("Help");
+        helpButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                helpButton2ActionPerformed(evt);
+            }
+        });
 
         solveButton.setBackground(new java.awt.Color(0, 153, 153));
         solveButton.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
@@ -276,6 +282,22 @@ public class AddingNumbersInterface extends javax.swing.JFrame {
         
         result.setText("");
     }//GEN-LAST:event_solveButtonActionPerformed
+
+    private void helpButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_helpButton2ActionPerformed
+        JOptionPane.showMessageDialog(null,"ABOUT THE GAME ADDING NUMBERS\n"+
+        "-The game Adding Numbers tries to test the player's mathematical abilities.\n"+
+        "-The game consists of solving sums, in which it is possible that no one of the 2 addends or the result will be shown on screen,\n"
+        +"this in order that the player is able to find the missing number and in this way find the solution to the problem.\n"+
+        "-The purpose of the game is for the player to enter in a table the number that he believes is the correct one to be able to solve the sum.\n"+
+        "-In case 1 you can find the first addend hidden.\n"+
+        "-In case 2 you can find the second hidden addend.\n"+
+        "-In case 3 you can find the result hidden.\n"+
+        "-In case 3 you can find the result hidden.\n"+
+        "-Each successful sum will allow the player to continue advancing.\n"+
+        "-If for some reason the player wishes to redo the addition, can try it with the option Again.\n"+
+        "-The game is won when the user manages to guess 15 sums and ends when the player decides to do it.");
+
+    }//GEN-LAST:event_helpButton2ActionPerformed
 
     /**
      * @param args the command line arguments
